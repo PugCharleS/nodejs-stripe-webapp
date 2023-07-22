99bcd8e00051b00aa1f0b5aab9c9c529658da30f
Carlos Solis <cmst010@gmail.com>
2023-07-22 02:06:21 -0600

    pasarela de pago implementada

---
+++ src/controllers/payment.controller.js
@@ -0,0 +1,33 @@
+import Stripe from "stripe";
+import { STRIPE_PRIVATE_KEY } from "../config.js";
+
+const stripe = new Stripe(STRIPE_PRIVATE_KEY);
+
+export const createSession = async (req, res) => {
+  const { productName, productPrice } = req.body;
+
+  try {
+    const session = await stripe.checkout.sessions.create({
+      line_items: [
+        {
+          price_data: {
+            product_data: {
+              name: productName,
+            },
+            currency: "usd",
+            unit_amount: productPrice,
+          },
+          quantity: 1,
+        },
+      ],
+      mode: "payment",
+      success_url: "http://localhost:3000/success",
+      cancel_url: "http://localhost:3000/cancel",
+    });
+
+    console.log(session);
+    return res.json({ url: session.url });
+  } catch (error) {
+    return res.status(500).json({ message: error.message });
+  }
+};
